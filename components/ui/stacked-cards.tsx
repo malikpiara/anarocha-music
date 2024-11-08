69c413d7cbf9e452356c1e9b357e7b7c358d0d29
Malik Piara <malik@hey.com>
2024-11-08 18:43:52 +0000

    Created component for link cards.

--- components/ui/stacked-cards.tsx
+++ components/ui/stacked-cards.tsx
@@ -1,26 +1,8 @@
 import Link from 'next/link';
 import { Card, CardContent } from './card';
+import { cards } from '@/content/linkCards';
 
 export default function CardStack() {
-  const cards = [
-    {
-      title: 'Music by Ana Rocha',
-      url: 'https://open.spotify.com/artist/4ppTNB1xdVMcSdgVXuAILM?si=KtwkehfiSuKswG4MWSySBw',
-    },
-    {
-      title: 'João dos Jornais - ELAS - Ana Rocha & Rui Salgado Trio',
-      url: 'https://youtu.be/warByl0LbLE',
-    },
-    {
-      title: 'Yearn with Diederik Wissels, Nicolas Kummert (Videoclip)',
-      url: 'https://www.youtube.com/watch?v=57Tkb90aHTQ',
-    },
-    {
-      title: 'Upcoming Concerts',
-      url: 'http://anarochagaspar.com/concerts/',
-    },
-  ];
-
   return (
     <div className='container mx-auto p-4 space-y-4'>
       {cards.map((card, index) => (
---
+++ content/linkCards.ts
@@ -0,0 +1,34 @@
+// This document stores all links you want to display as cards.
+// Each card has a name (title) and a URL.
+
+export const cards = [
+  {
+    title: 'Music by Ana Rocha',
+    url: 'https://open.spotify.com/artist/4ppTNB1xdVMcSdgVXuAILM?si=KtwkehfiSuKswG4MWSySBw',
+  },
+  {
+    title: 'João dos Jornais - ELAS - Ana Rocha & Rui Salgado Trio',
+    url: 'https://youtu.be/warByl0LbLE',
+  },
+  {
+    title: 'Yearn with Diederik Wissels, Nicolas Kummert (Videoclip)',
+    url: 'https://www.youtube.com/watch?v=57Tkb90aHTQ',
+  },
+  {
+    title: 'Upcoming Concerts',
+    url: 'http://anarochagaspar.com/concerts/',
+  },
+];
+
+/*
+How to add new link cards:
+1. Copy one of the blocks below (from the opening { to the closing }),
+2. Paste it after the last item, but before the final ].
+3. Change the values for 'title' and 'url' to match the new item.
+
+Example:
+{
+  title: 'Yearn with Diederik Wissels', // Title to be displayed.
+  url: 'https://www.youtube.com/watch?v=57Tkb90aHTQ', // Link.
+}
+*/
